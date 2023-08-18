79422035073d1c3503bd3bdc534c94eb982ab2bc
Brian De Winne <dewinnebrian@gmail.com>
2023-08-18 14:18:17 +0200

    scroll to top button fix

--- js/scripts.js
+++ js/scripts.js
@@ -7,24 +7,46 @@ scrollTopBtn.style.display = 'none';
 scrollTopBtn.style.border = 'none';
 scrollTopBtn.style.background = 'none';
 scrollTopBtn.style.cursor = 'pointer';
-scrollTopBtn.setAttribute('data-aos', 'fade-up'); // Add this line
+scrollTopBtn.setAttribute('data-aos', 'fade-up');
 document.body.appendChild(scrollTopBtn);
 
 var arrowImage = document.createElement('img');
-arrowImage.src = '/TestPage/img/arrow.png'; // Adjust the path to the location of your image
-arrowImage.style.width = '40px'; // Adjust the size to fit your design
+arrowImage.src = '/TestPage/img/arrow.png';
+arrowImage.style.width = '40px';
 arrowImage.style.height = '40px';
-arrowImage.setAttribute('data-aos', 'fade-up'); // Add this line
+arrowImage.setAttribute('data-aos', 'fade-up');
 scrollTopBtn.appendChild(arrowImage);
 
 window.addEventListener('scroll', function() {
     if (window.scrollY > 450) {
         scrollTopBtn.style.display = 'block';
-        AOS.refresh(); // Add this line
+        AOS.refresh();
     } else {
         scrollTopBtn.style.display = 'none';
     }
+
+    var scrollTopBtn = document.getElementById("scrollTopBtn");
+    var footer = document.querySelector(".footer");
+    var footerRect = footer.getBoundingClientRect();
+    var navbar = document.querySelector(".navbar");
+    var navbarRect = navbar.getBoundingClientRect();
+    var arrowBottom = window.innerHeight - footerRect.top + 20;
+
+    if (footerRect.top < window.innerHeight) {
+      scrollTopBtn.style.bottom = arrowBottom + "px";
+    } else {
+      scrollTopBtn.style.bottom = "30px";
+    }
+
+    var arrowTop = navbarRect.bottom + 20;
+
+    if (navbarRect.bottom > 0) {
+      scrollTopBtn.style.top = arrowTop + "px";
+    } else {
+      scrollTopBtn.style.top = "auto";
+    }
 });
+
 scrollTopBtn.addEventListener('click', function() {
     window.scrollTo({
         top: 0,
@@ -32,32 +54,18 @@ scrollTopBtn.addEventListener('click', function() {
     });
 });
 
-window.addEventListener("scroll", function () {
-    var scrollTopBtn = document.getElementById("scrollTopBtn");
-    var footer = document.querySelector(".footer");
-    var footerRect = footer.getBoundingClientRect();
-    var arrowBottom = window.innerHeight - footerRect.top + 20;
-  
-    if (footerRect.top < window.innerHeight) {
-      scrollTopBtn.style.bottom = arrowBottom + "px";
-    } else {
-      scrollTopBtn.style.bottom = "20px";
-    }
-  });
-
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const text = "A Wieland holding company.";
     const element = document.getElementById("typedText");
     let index = 0;
-  
+
     function typeText() {
       if (index < text.length) {
         element.textContent += text.charAt(index);
         index++;
-        setTimeout(typeText, 30); // Speed of typing effect
+        setTimeout(typeText, 30);
       }
     }
-  
-    typeText();
-  });
 
+    typeText();
+});
