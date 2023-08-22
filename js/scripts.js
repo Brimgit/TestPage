954c1c3604de9cd69b887f367478e25ae86a3657
Brian De Winne <dewinnebrian@gmail.com>
2023-08-22 10:58:16 +0200

    refining

--- js/scripts.js
+++ js/scripts.js
@@ -1,89 +1,84 @@
-var scrollTopBtn = document.createElement('button');
-scrollTopBtn.id = 'scrollTopBtn';
-scrollTopBtn.style.position = 'fixed';
-scrollTopBtn.style.bottom = '30px';
-scrollTopBtn.style.right = '30px';
-scrollTopBtn.style.display = 'none';
-scrollTopBtn.style.border = 'none';
-scrollTopBtn.style.background = 'none';
-scrollTopBtn.style.cursor = 'pointer';
-scrollTopBtn.setAttribute('data-aos', 'fade-up');
-document.body.appendChild(scrollTopBtn);
-
-var arrowImage = document.createElement('img');
-arrowImage.src = '/TestPage/img/arrow.png';
-arrowImage.style.width = '40px';
-arrowImage.style.height = '40px';
-arrowImage.setAttribute('data-aos', 'fade-up');
-scrollTopBtn.appendChild(arrowImage);
-
-window.addEventListener('scroll', function() {
-    var scrollTopBtn = document.getElementById("scrollTopBtn");
-    var footer = document.querySelector(".footer");
-    var footerRect = footer.getBoundingClientRect();
-    var navbar = document.querySelector(".navbar");
-    var navbarRect = navbar.getBoundingClientRect();
-
-    // Show the button if scroll position is greater than 450px
-    if (window.scrollY > 450) {
+function createScrollTopButton() {
+    var scrollTopBtn = document.createElement('button');
+    scrollTopBtn.id = 'scrollTopBtn';
+    scrollTopBtn.style.position = 'fixed';
+    scrollTopBtn.style.bottom = '30px';
+    scrollTopBtn.style.right = '30px';
+    scrollTopBtn.style.display = 'none';
+    scrollTopBtn.style.border = 'none';
+    scrollTopBtn.style.background = 'none';
+    scrollTopBtn.style.cursor = 'pointer';
+    document.body.appendChild(scrollTopBtn);
+  
+    var arrowImage = document.createElement('img');
+    arrowImage.src = '/TestPage/img/arrow.png';
+    arrowImage.style.width = '40px';
+    arrowImage.style.height = '40px';
+    scrollTopBtn.appendChild(arrowImage);
+  
+    window.addEventListener('scroll', function() {
+      var scrollTopBtn = document.getElementById("scrollTopBtn");
+      var footer = document.querySelector(".footer");
+      var footerRect = footer.getBoundingClientRect();
+      var navbar = document.querySelector(".navbar");
+      var navbarRect = navbar.getBoundingClientRect();
+  
+      // Show the button if scroll position is greater than 450px
+      if (window.scrollY > 450) {
         scrollTopBtn.style.display = 'block';
-    } else {
+      } else {
         scrollTopBtn.style.display = 'none';
-    }
-
-    // Adjust the bottom position to avoid overlapping with the footer
-    var arrowBottom = window.innerHeight - footerRect.top + 20;
-    if (footerRect.top < window.innerHeight) {
+      }
+  
+      // Adjust the bottom position to avoid overlapping with the footer
+      var arrowBottom = window.innerHeight - footerRect.top + 20;
+      if (footerRect.top < window.innerHeight) {
         scrollTopBtn.style.bottom = arrowBottom + "px";
-    } else {
+      } else {
         scrollTopBtn.style.bottom = "30px";
-    }
-
-    // Hide the button if it overlaps with the navbar
-    if (navbarRect.bottom > 0 && scrollTopBtn.getBoundingClientRect().top < navbarRect.bottom) {
+      }
+  
+      // Hide the button if it overlaps with the navbar
+      if (navbarRect.bottom > 0 && scrollTopBtn.getBoundingClientRect().top < navbarRect.bottom) {
         scrollTopBtn.style.display = 'none';
-    }
-});
-
-scrollTopBtn.addEventListener('click', function() {
-    window.scrollTo({
+      }
+    });
+  
+    scrollTopBtn.addEventListener('click', function() {
+      window.scrollTo({
         top: 0,
         behavior: 'smooth'
+      });
     });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    createScrollTopButton();
+  
     const text = "A Wieland holding company.";
     const element = document.getElementById("typedText");
     let index = 0;
-
+  
     function typeText() {
-        if (index < text.length) {
-            element.textContent += text.charAt(index);
-            index++;
-            setTimeout(typeText, 30);
-        }
+      if (index < text.length) {
+        element.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeText, 30);
+      }
     }
-
+  
     typeText();
-});
-
-$(document).ready(function() {
-    // Reference to the mega menu parent element
-    const megaMenuParent = $('.mega-dropdown');
-  
-    // Reference to the mega menu
-    const megaMenu = $('.mega-menu');
-  
-    // Handle hovering over the mega menu parent
-    megaMenuParent.on('mouseenter', function() {
-      megaMenu.show(); // Show the mega menu
-    });
-  
-    // Handle moving the cursor out of the mega menu parent
-    megaMenuParent.on('mouseleave', function() {
-      megaMenu.hide(); // Hide the mega menu
-    });
   });
   
+  $(document).ready(function() {
+    const megaMenuParent = $('.mega-dropdown');
+    const megaMenu = $('.mega-menu');
   
+    megaMenuParent.on('mouseenter', function() {
+      megaMenu.show();
+    });
+  
+    megaMenuParent.on('mouseleave', function() {
+      megaMenu.hide();
+    });
+  });
